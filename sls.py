9399ee8af97e0204bc6ffc29a504f46159d4ea7d
kftang <kennyftang@gmail.com>
2022-12-08 00:37:19 -0800

    Added SLS code and updated notebook

---
+++ sls.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import numpy as np
+import math
+
+def heuristic(g: nx.Graph, cur_node_idx: int, next_node_idx: int) -> float:
+  cur_next_dist = g[cur_node_idx][next_node_idx]['weight']
+  return cur_next_dist
+
+
+def sls(g: nx.Graph) -> 'List[node]':
+  solution = [0]
+  visited = {0}
+  last_node = 0
+  while len(solution) != len(g.nodes):
+    min_node = None
+    min_score = math.inf
+    for node in range(len(g.nodes)):
+      if node in visited:
+        continue
+      score = heuristic(g, last_node, node)
+      if score < min_score:
+        min_node = node
+        min_score = score
+
+    visited.add(min_node)
+    solution.append(min_node)
+    last_node = min_node
+  return solution
--- tsp.py
+++ tsp.py
@@ -5,16 +5,18 @@ from pathlib import Path
 import networkx as nx
 import numpy as np
 
+from sls import sls
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('input_file', type=str)
-  parser.add_argument('-s', help='sls algorithm', type=bool)
+  parser.add_argument('-s', help='sls algorithm', action='store_true')
   
   # parse args to find file
   args = parser.parse_args()
   input_path = Path(args.input_file)
   if not input_path.exists():
-    print(f'Input file {input_file} does not exist')
+    print(f'Input file {args.input_file} does not exist')
     sys.exit()
 
   # parse input file
@@ -22,6 +24,19 @@ if __name__ == '__main__':
     num_nodes = int(input_file.readline())
     adj_matrix = np.empty((num_nodes, num_nodes))
     for i in range(num_nodes):
-      adj_matrix[i] = list(map(float, input_file.readline().split()))
+      distances = map(float, input_file.readline().split())
+      for j, distance in enumerate(distances):
+        adj_matrix[i][j] = distance
 
-  graph = nx.from_numpy_array(adj_matrix)
+  g = nx.from_numpy_array(adj_matrix)
+
+  opt_path = nx.approximation.traveling_salesman_problem(g, cycle=False)
+  print(f'Optimal path: {opt_path}')
+  opt_cost = nx.path_weight(g, opt_path, 'weight')
+  print(f'Cost: {opt_cost}')
+
+  if args.s:
+    path = sls(g)
+    print(f'Found path: {path}')
+    cost = nx.path_weight(g, path, 'weight')
+    print(f'Cost: {cost}')
